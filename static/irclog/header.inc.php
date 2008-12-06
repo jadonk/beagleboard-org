f7c94c75b9d95c26057fbc40853185628c6ee894
Jason Kridner <jdk@ti.com>
2008-12-05 21:31:30 -0600

    Updated stylesheet from http://rbach.priv.at/Microformats/IRC/style.css.

--- static/irclog/header.inc.php
+++ static/irclog/header.inc.php
@@ -14,29 +14,7 @@
 <meta name="description" content="IRC Log for <?php echo($channel); ?>" />
 <meta name="keywords" content="IRC Log for <?php echo($channel); ?>" />
 <script src="/static/irclog/script.js" type="text/javascript"> </script>
-
-<style type="text/css">
-body {
-    font-family: Verdana, Arial, Helvetica, sans-serif;
-    font-size: 12px;
-    color: #000000;
-}
-cite,q{font-style:normal}
-ol,li{margin:0;padding:0;}
-ol{list-style-type: disc;}
-ol.log{list-style-type:none;}
-q:before,q:after{content:""};
-cite{font-style:normal;}
-abbr{font-family: Courier New, Courier, mono; border:0;}
-.topic span, .mode span, .nick-change span, .join span {color: #009200;}
-.message {}
-.notice span {color: #7b0000;}
-.part span, .quit span {color: #00007b;}
-.action span {color: #9c009c;}
-.topic-change span {font-weight:bold;}
-.ping span {color: #ff0000;}
-</style>
-
+<link rel="stylesheet" href="/static/irclog/style.css" />
 </head>
 
 <body>
