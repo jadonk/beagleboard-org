7e90d257747588ee123a0ba8d072724165018907
Jason Kridner <jdk@ti.com>
2014-04-10 21:20:53 +0000

    updater.js: updates

--- static/updater.js
+++ static/updater.js
@@ -1,49 +1,90 @@
 var fs = require('fs');
-var http = require('http');
+var net = require('net');
 var b = require('bonescript');
+var socketio = require('bonescript/node_modules/socket.io');
 
+// Setup LED indicators
 var state = 'init';
 var phase = 0;
 var leds = ['USR0', 'USR1', 'USR2', 'USR3'];
 for(var i in leds) {
     b.pinMode(leds[i], b.OUTPUT);
-    b.digitalWrite(leds[i]. b.HIGH);
+    b.digitalWrite(leds[i], b.HIGH);
 }
 setInterval(updateLEDs, 100);
 
-var verifyContents = {};
-verifyContents.date = new Date();
-verifyContents.platform = b.getPlatform();
-verifyContents.mounts = fs.readFileSync('/proc/mounts');
+// Setup connection listener
+var port = 4999;
+getPort();
 
-var req = http.get("http://beagleboard.org/latest-images/", onGetLatestImages);
-req.on('error', onGetLatestImagesError);
+// Timeout on no connect for 2 minutes
+var timeout = setTimeout(onDisconnect, 2*60*1000);
 
-function onGetLatestImages(res) {
-    console.log("Got response: " + res.statusCode);
-    fs.writeFileSync('/tmp/verify-updater.json', JSON.stringify(verifyContents, null, 4));
+function onConnection(socket) {
+    clearTimeout(timeout);
+    state = 'connected';
+    var connectDate = new Date();
+    socket.emit('connect', { date: connectDate, port: port });
+    socket.on('mounts', onMounts);
+    socket.on('disconnect', onDisconnect);
+
+    function onMounts(data) {
+        fs.readFile('/proc/mounts', 'ascii', onMountsFile);
+    }
+    
+    function onMountsFile(error, data) {
+        socket.emit('mounts', {error: error, data: data});
+    }
 }
-
-function onGetLatestImagesError(e) {
-    console.log("Got error: " + e.message);
+    
+function onDisconnect() {
+    state = 'disconnected';
+    restoreLEDs();
+    fs.unlinkSync(__filename);
+    process.exit(0);
 }
 
 function updateLEDs() {
     if(state == 'init') {
-        phase++;
-        phase = phase % 8;
+        phase = (phase + 1) % 8;
         if(phase == 0) {
-            for(var i in leds) {
-                b.pinMode(leds[i], b.OUTPUT);
-                b.digitalWrite(leds[i]. b.HIGH);
-            }
-        } else if(phase == 4) {
-            for(var i in leds) {
-                b.pinMode(leds[i], b.OUTPUT);
-                b.digitalWrite(leds[i]. b.LOW);
-            }
+            setLEDs(b.HIGH);
+        } else if(phase == 6) {
+            setLEDs(b.LOW);
         }
-    } else if(phase == 'waiting') {
+    } else if(state == 'connected') {
+        phase = (phase + 1) % 8;
+        if(phase == 0) {
+            setLEDs(b.HIGH);
+        } else if(phase == 1) {
+            setLEDs(b.LOW);
+        }
     } else {
     }
+}
+
+function setLEDs(level) {
+    for(var i in leds) {
+        b.digitalWrite(leds[i], level);
+    }
+}
+
+function restoreLEDs() {
+    var p = '/sys/class/leds/beaglebone:green:usr';
+    b.digitalWrite('USR0', b.LOW);
+    b.digitalWrite('USR1', b.LOW);
+    b.digitalWrite('USR2', b.LOW);
+    b.digitalWrite('USR3', b.LOW);
+    b.writeTextFile(p+'0/trigger', 'heartbeat');
+    b.writeTextFile(p+'1/trigger', 'mmc0');
+    b.writeTextFile(p+'2/trigger', 'cpu0');
+    b.writeTextFile(p+'3/trigger', 'mmc1');
+}
+
+function getPort() {
+    port++;
+    console.log('Trying port ' + port);
+    var io = socketio.listen(port);
+    io.on('error', getPort);
+    io.sockets.on('connection', onConnection);
 }
