58915a311f6aaffdc534c8ef20012780e0da8c27
Jason Kridner <jdk@ti.com>
2014-08-06 22:15:01 +0000

    jekyll: conversion script reading in db

--- node/jekyll/convert_db.js
+++ node/jekyll/convert_db.js
@@ -1,3 +1,38 @@
 #!/usr/bin/env node
 console.log('Starting convert_db.js');
+var xml2js = require('xml2js');
+var fs = require('fs');
+
+var parser = xml2js.Parser();
+var filepath = __dirname + '/../../db/';
+var db = {};
+
+addElements('/', 0, onDone);
+
+function addElements(path, id, callback) {
+    fs.readFile(filepath + id + '.xml', onFile);
+
+    function onFile(err, data) {
+        if(err) console.log('Error: ' + err);
+        parser.parseString(data, onParse);
+    }
+
+    function onParse(err, result) {
+        console.log('parsed ' + id);
+        //console.log(id + ':' + JSON.stringify(result, null, '\t'));
+        var children = result.xmlroot.hopobject[0]['hop:child'];
+        for(var i in children) {
+            var cid = children[i]['$'].idref;
+            console.log('adding ' + cid);
+            addElements(path + i + '/', cid, testForDone);
+        }
+    }
+}
+
+function testForDone() {
+}
+
+function onDone() {
+    console.log(JSON.stringify(db, null, '\t'));
+}
 
