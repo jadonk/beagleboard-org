e29eaad709ef8fa4088ff4532196ded44ca713d4
Jason Kridner <jdk@ti.com>
2014-12-12 00:49:23 +0000

    changes

--- node/jekyll/convert_db.js
+++ node/jekyll/convert_db.js
@@ -2,16 +2,19 @@
 console.log('Starting convert_db.js');
 var xml2js = require('xml2js');
 var fs = require('fs');
+var mkdirp = require('mkdirp');
+var path = require('path');
 
 var parser = xml2js.Parser();
 var filepath = __dirname + '/../../db/';
+var dstpath = __dirname + '/src';
 var db = {};
 
 addElements('/', 0);
 process.on('exit', onDone);
 
-function addElements(path, id) {
-    console.log('adding ' + path + id);
+function addElements(epath, id) {
+    console.log('adding ' + epath + id);
     fs.readFile(filepath + id + '.xml', onFile);
 
     function onFile(err, data) {
@@ -22,9 +25,20 @@ function addElements(path, id) {
     function onParse(err, result) {
         var uri = result.xmlroot.hopobject[0]['uri'];
         if(!uri) uri = id;
-        var newpath = path + uri + '/';
+        var newpath = epath + uri + '/';
         db[newpath] = result;
         console.log('parsed ' + id + ':' + newpath);
+        var body = result.xmlroot.hopobject[0]['body'];
+        if(body) {
+            var layout = result.xmlroot.hopobject[0]['render_skin'];
+            var yaml = '' +
+                '---\n' +
+                'layout: ' + layout + '\n' +
+                'title: ' + uri + '\n' +
+                '---\n' +
+                body[0];
+            writeFile(dstpath + newpath + 'index.html', yaml, onWrite);
+        }
         //console.log(id + ':' + JSON.stringify(result, null, '\t'));
         var children = result.xmlroot.hopobject[0]['hop:child'];
         for(var i in children) {
@@ -32,9 +46,23 @@ function addElements(path, id) {
             addElements(newpath, cid);
         }
     }
+
+    function onWrite(err) {
+        console.log('write err (' + id + '): ' + err);
+    }
 }
 
 function onDone() {
     console.log(JSON.stringify(db, null, '\t'));
+}
+
+function writeFile(filename, contents, callback) {
+    var mypath = path.dirname(filename);
+    mkdirp(mypath, onMkdir);
+
+    function onMkdir(err) {
+        if(err) return callback(err);
+        fs.writeFile(filename, contents, callback);
+    }
 }
 
